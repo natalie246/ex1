c1cbb77b5db001681851c6d84a67d3d502998ec5
natalie246 <natalieakiva@gmail.com>
2015-06-17 22:26:32 +0300

    ex1bookstore

--- book_ws.js
+++ book_ws.js
@@ -8,6 +8,8 @@ var port = process.env.PORT || 3000;
 app.listen(port);
 console.log("listening on port " + port);
 
+
+
 var myStore = bookStore.getBookStore("stimatzki");
 myStore.setBooksFromJSON();
 
@@ -18,21 +20,29 @@ res.send("server root");
 
 app.get('/printAllBooks',function (req,res){ 
 	app.set('json space',3);
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin,X-Requested-With, Content-Type, Accept");
 		res.json(myStore.printAllBooks());
 
 });
 
 app.get('/getBookPriceByName/:bookName',function (req,res){ 
 	app.set('json space',3);
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin,X-Requested-With, Content-Type, Accept");
 		res.json(myStore.getBookPriceByName(req.params.bookName));
 });
 
 app.get('/getAllBooksByCategory/:bookCategory',function (req,res){ 
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin,X-Requested-With, Content-Type, Accept");
 	app.set('json space',3);
-		res.json(myStore.getAllBooksByCategory(req.params.bookCategory));
+	res.json(myStore.getAllBooksByCategory(req.params.bookCategory));
 });
 
 app.get('/getMostExpensiveBook',function (req,res){ 
 	app.set('json space',3);
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin,X-Requested-With, Content-Type, Accept");
 		res.json(myStore.getMostExpensiveBook());
 });
